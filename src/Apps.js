c7db752cc7f2e73e5439964999d4d68e59c167c6
AidenBedio <aidenbedio@gmail.com>
2021-11-05 18:59:47 +0800

    added Detail.js modified Pets.js, Apps.js Added Router, Route

--- src/Apps.js
+++ src/Apps.js
@@ -1,11 +1,23 @@
+import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
       <h1 id="my-brand">Adopt Me!</h1>
-      <SearchParams></SearchParams>
+      <Router>
+        <Switch>
+          <Route path="/details/:id">
+            <Details></Details>
+          </Route>
+          <Route path="/">
+            <SearchParams></SearchParams>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
@@ -31,4 +43,9 @@ const App = () => {
 //   ]);
 // };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
